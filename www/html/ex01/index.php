ea76ce30001822e1fa54bb462a09fc4e1da1bc52
細田空冶 <qooooo@hosodakuunoAir.AirPort>
2022-02-08 02:33:27 +0900

    Initial

---
+++ www/html/ex01/index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+$_SESSION['opinion'] = $_GET['opinion'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <p>変換したい文字列(制限値：1042byte)</p>
+    <p><textarea name="opinion" cols="50" rows="5">
+<?php echo($_SESSION['opinion']); ?>
+</textarea></p>
+    <p><input type="submit" name="submitBtn" value="　⬇変換する⬇　"></p>
+  </form>
+
+<?php
+$str = $_POST['opinion'];
+$STR = strtoupper($str);
+?>
+
+
+    <p>変換された文字列</p>
+    <p><textarea name="opinion" cols="50" rows="5">
+<?php
+if(strlen($str) > 1024){
+    echo "制限値オーバー";
+}else{
+echo $STR;
+    }
+
+?>
+</textarea></p>
+
+</body>
+</html>
+
+
---
+++ www/html/ex02/index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+<script>
+  window.onload = function () {
+  
+  // textarea要素を取得
+  var hoge = document.getElementById("textarea");
+  // 表示用
+  var obj = document.getElementsByClassName('badge-primary')[0];
+  // 入力時に発生
+  hoge.addEventListener('input', (event) => {
+    //　入力された値を表示
+    obj.textContent = hoge.value.replace(/{/g, " {\n  ").replace(/;/g, ";\n  ").replace(/}/g, "}\n").replace(/:/g, ": ");
+    $_SESSION['before'] = hoge.value;
+
+  });
+
+
+  }
+</script>
+</head>
+
+<body>
+
+  <form action="index.php" method="get">
+    <p>変換前のコード</p>
+    <textarea name="opinion" cols="50" class="form-control" rows="5" id="textarea">
+      <?php echo($_SESSION['before']); ?>
+    </textarea>
+  </form>
+
+
+
+
+
+
+    <p>変換された文字列</p>
+<textarea name="opinion" cols="50" rows="5" class="badge badge-primary">
+
+</textarea>
+
+</body>
+</html>
+
+
+
